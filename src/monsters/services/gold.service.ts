2477fb6780a6e0692aac9205fccbfee68f615d8f
Jandro <jandroponpal@gmail.com>
2023-12-15 15:32:08 +0100

    created error handling in gold.service

--- src/monsters/services/gold.service.ts
+++ src/monsters/services/gold.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Monster } from '../entities/monster.entity';
 import { Model } from 'mongoose';
@@ -11,12 +11,25 @@ export class GoldService {
 
   async addGold(id: string, amount: number): Promise<Monster> {
     const monster = await this.monsterModel.findById(id);
+    if (!monster) {
+        throw new NotFoundException(`Monster with ID ${id} not found`);
+      }
+
     monster.goldBalance += amount;
     return monster.save();
   }
 
   async removeGold(id: string, amount: number): Promise<Monster> {
     const monster = await this.monsterModel.findById(id);
+
+    if (!monster) {
+        throw new NotFoundException(`Monster with ID ${id} not found`);
+      }
+
+    if (monster.goldBalance - amount < 0) {
+    throw new BadRequestException(`Cannot make the monster go into debt!`);
+    }
+
     monster.goldBalance -= amount;
     return monster.save();
   }
